95fa990f3c3037ee5439d1fb063055091224491a
Danilo Carvalhal dos Santos Pedrosa <danilocarvalhal123@gmail.com>
2023-05-10 23:31:10 -0300

    Update app.js

--- simple_server/app.js
+++ simple_server/app.js
@@ -4,6 +4,7 @@ bodyParser = require('body-parser'),
 cors = require('cors'),
 mongoose = require('mongoose');
 
+
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1:27017/mean', { useNewUrlParser: true, useUnifiedTopology: true }).then(
   () => {console.log('Database is connected') },
